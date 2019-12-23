6cad517f9efaee77d8a19e5de7057486f7f2a74b
aviva.tang <18603050952@163.com>
2019-12-23 22:21:26 +0800

    【优化】注销用例兼容不同分辨率

--- src/test/java/app/pages/mine/MinePage.java
+++ src/test/java/app/pages/mine/MinePage.java
@@ -3,11 +3,12 @@ package app.pages.mine;
 import app.pages.App;
 import app.pages.BasePage;
 import org.openqa.selenium.By;
+import utils.ScrollUtils;
 
 public class MinePage extends BasePage {
 
     public App logout() {
-//        TODO scroll
+        ScrollUtils.swape(driver,0.5,0.8,0.5,0.5);
         click(By.id("com.tencent.wework:id/elb"));
         click(By.id("com.tencent.wework:id/elf"));
         click(By.id("com.tencent.wework:id/at7"));
